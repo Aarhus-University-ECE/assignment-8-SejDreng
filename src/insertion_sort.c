a788d89546944e355616e88f16d8de4cc891db1a
SejDreng <114228762+SejDreng@users.noreply.github.com>
2022-10-27 16:10:39 +0200

    Update

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,9 +1,56 @@
 #include "insertion_sort.h"
 #include "linked_list.h"
 
-
+// Creating my own switch function to switch the data-values.
+void byt (node_t *x, node_t *y)
+    {
+        int temp = x->data;
+        x->data = y->data;
+        y->data = temp;
+    }
 
 void sort(linked_list *llPtr)
 {
- // Add your sort function here
+// Add your sort function here
+
+// Creating a few pointers to keep track of the position in the linked list.
+node_t *pre = llPtr->head;
+node_t *cur = llPtr->head->next;
+node_t *track = llPtr->head;
+
+// The main while loop, which will sort the entire linked list.
+while(cur != NULL)
+{
+    /*
+    If the data in the two current elements is the same or already in the right order, 
+    then we will move on to the next element.
+    */
+    if(pre->data < cur->data || pre->data == cur->data)
+    {
+        pre = cur;
+        cur = pre->next;
+        continue;
+    }
+    /*
+    If the current element indeed is smaller than the previous one, 
+    then we want to moove it until it is in the right position.
+    */
+    if(pre->data > cur->data)
+    {
+        /*
+        Positioning my tracker to the desired position right before my previous element, 
+        so that i can use it to move my pointers.
+        */
+        while (track != pre && track->next != pre)
+        {
+            track = track->next;
+        }
+        //Switching the data and mooving my pointers so they are ready to execute the sequence again.
+        byt(pre,cur);
+        pre = track;
+        cur = pre->next;
+        track = llPtr->head;
+        continue;
+    }
+}
 }
--- src/list_queue.c
+++ src/list_queue.c
@@ -1,22 +1,35 @@
 #include "list_queue.h"
 
-
 void init_queue(queue *q)
 {
   // Add your init_queue
+  q->size = 0;
+  q->front = NULL;
+  q->rear = NULL;
 }
 
 int empty(queue *q)
 {
-  // Add your empty function  
+  if(q->size == 0){
+    return(1);
+  } 
+  else{
+    return(0);
+  }
 }
 
 void enqueue(queue *q, int x)
 {
-  // Add your enqueue function
+  qnode *second_last = malloc(sizeof(qnode));
+  second_last = q->rear;
+  q->rear->next = second_last;
+  q->rear->data = x;
 }
 
 int dequeue(queue *q)
 {
   // Add your dequeue function
+  int temp = q->front->data;
+  while 
+  return(temp)
 }
