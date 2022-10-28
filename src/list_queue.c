ea1fcf749c06840813f6076a7aa9a301deb27984
SejDreng <114228762+SejDreng@users.noreply.github.com>
2022-10-28 11:23:21 +0200

    Update

--- src/list_queue.c
+++ src/list_queue.c
@@ -1,5 +1,6 @@
 #include "list_queue.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 void init_queue(queue *q)
 {
@@ -22,26 +23,68 @@ int empty(queue *q)
 void enqueue(queue *q, int x)
 {
   qnode *second_last = malloc(sizeof(qnode));
-  second_last = q->rear;
-  q->rear->next = second_last;
-  q->rear->data = x;
+  if(empty(q)==0){
+    second_last = q->rear;
+    q->rear->next = second_last;
+    q->rear->data = x;
+  }
+  else
+  {
+    second_last->data = x;
+    second_last->next = NULL;
+    q->front = second_last;
+    q->rear = second_last;
+  }
+  q->size++;
 }
 
 int dequeue(queue *q)
 {
+  /*
   // Add your dequeue function
+  if(empty(q) == 0){
   int temp = q->front->data;
   qnode *finder = malloc(sizeof(qnode));
   finder->next = q->rear;
   while(finder->next != q->front)
   {
     finder->next = finder->next->next;
-    if (finder->next==finder->next->next) {
-      printf("bruh");
-    }
   }
   q->front = finder;
   q->front->next = NULL;
+  q->size = q->size - 1;
+  }
+  else
+  printf("dude, is empty");
 
-  return(temp);
+
+
+  return ();*/
+
+  if (empty(q)==1) {
+    return 0;
+  }
+
+  int return_value = q->front->data;
+
+  qnode *finder = malloc(sizeof(qnode));
+  finder = q->rear;
+  while (finder->next != q->front) {
+    finder->next=finder->next->next;
+  }
+
+  if (q->size == 1){
+    q->size=0;
+    q->front=NULL;
+    q->rear=NULL;
+    return return_value;
+  } else {
+    q->front=finder;
+    q->front->next=NULL;
+    q->size-=1;
+    return return_value;
+  }
+
+
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,55 @@
 extern "C"{
     #include "insertion_sort.h"
     #include "linked_list.h"
+    #include "list_queue.h"
+}
+
+void test_q (queue *q, int x, int x0, int x1, int y, int y0, int y1)
+{
+    queue *temp = q;
+
+    init_queue(temp);
+    if (empty(temp) == 1)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    temp = q;
+
+    enqueue(temp,x); y = dequeue(temp);
+    if (temp == q && x == y)
+    {
+        printf("true");
+    }
+    else 
+    {
+        printf("false");
+    }
+
+    enqueue(temp, x0); enqueue(temp, x1);
+    y0 = dequeue(temp); y1 = dequeue(temp);
+
+    if(temp == temp)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    printf("%d is %d, and %d is %d\n",x0,y0,x1,y1);
+
+
+
 }
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
-
+    
     int testlist[] ={4,3,10,1,6};
     linked_list* test = createLinkedList();
     for (int i = 0; i < 5; ++i)
@@ -17,7 +60,17 @@ int main(int argc, char **argv)
     printLL(test);
     sort(test);
     printLL(test);
-    
+
+
+    int x = 1;
+    int x0 = 2;
+    int x1 = 3; 
+    int y = 4;
+    int y0 = 5;
+    int y1 = 6;
+    queue *q;
+
+    test_q(q,x,x0,x1,y,y0,y1);
     return 0;
 }
 
